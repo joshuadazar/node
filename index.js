e864e0252712622db34f9ca58d05345638870895
joshua daza rincon <joshuadazar@gmail.com>
2020-12-30 00:40:59 -0500

    change port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/home', (req, res) => {
     res.send('<h1>hola</h1><button>aceptar</button>');
 })
 
-app.listen(3000, () => {
+app.listen(3000) 
     
     console.log('server en puerto 3000 y seguirá');
-})
+
