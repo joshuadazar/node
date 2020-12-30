466e77f74d4c4de3001d14e1543556bbda6f62a0
joshua daza rincon <joshuadazar@gmail.com>
2020-12-30 00:28:24 -0500

    change port

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express= require('express')
 const app = express()
 
 app.get('/home', (req, res) => {
-    res.sendFile(__dirname+"/index.html");
+    // res.sendFile(__dirname+"/index.html");
+    res.send('<h1>hola</h1><button>aceptar</button>');
 })
 
 app.listen(3000, () => {
